df8a6d659b0bb1d7569c1aaad573102725a1f341
Khayala <m.xayala@outlook.com>
2023-02-15 18:59:06 +0400

    env

--- src/common/Api/globalApi.js
+++ src/common/Api/globalApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const customFetch = axios.create({
-    baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit'
+    baseURL: process.env.REACT_APP_API_URL
 })
